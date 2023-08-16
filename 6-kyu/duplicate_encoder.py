f778d6ccde0d83575a6a95dc93e87f53619272dd
clmiranda <miranda76575@gmail.com>
2023-08-16 00:24:13 -0400

    Added 'Duplicate Encoder' solution

---
+++ 6-kyu/duplicate_encoder.py
@@ -0,0 +1,6 @@
+# Duplicate Encoder => https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+
+
+def duplicate_encode(word: str):
+    word = word.lower()
+    return "".join(")" if word.count(x) > 1 else "(" for x in word)
