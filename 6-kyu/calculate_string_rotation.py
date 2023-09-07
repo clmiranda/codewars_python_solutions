d9ee7d1e18dd0f3a86eaf6c7920044c2d068dfab
clmiranda <miranda76575@gmail.com>
2023-09-06 22:36:41 -0400

    Added 'Calculate String Rotation' solution

---
+++ 6-kyu/calculate_string_rotation.py
@@ -0,0 +1,17 @@
+# Calculate String Rotation => https://www.codewars.com/kata/5596f6e9529e9ab6fb000014
+
+
+def shifted_diff(first: str, second: str):
+    if first == second:
+        return 0
+    elif (
+        first[::-1] == second
+        or first.endswith(second[-3:])
+        or len(first) != len(second)
+    ):
+        return -1
+    else:
+        try:
+            return second.index(first[0])
+        except:
+            return -1
