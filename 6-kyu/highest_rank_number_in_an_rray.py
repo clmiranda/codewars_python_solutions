7da39d176359d7812540f353128840228543167e
clmiranda <miranda76575@gmail.com>
2023-11-21 10:54:37 -0400

    Added 'Highest Rank Number in an Array' solution

---
+++ 6-kyu/highest_rank_number_in_an_rray.py
@@ -0,0 +1,9 @@
+# Highest Rank Number in an Array => https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004
+
+from collections import Counter
+
+
+def highest_rank(arr):
+    arr_counter = Counter(arr)
+    v_max = max([x for x in arr_counter.values()])
+    return max([x for x in filter(lambda v: arr_counter[v] == v_max, arr)])
